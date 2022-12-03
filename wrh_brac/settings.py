55eeb240eb1c5699d4cccb12fd69df17d2c5fe92
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-12-03 21:28:04 +0530

    html code push

--- wrh_brac/settings.py
+++ wrh_brac/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['165.232.188.93']
+ALLOWED_HOSTS = ['165.232.188.93', 'http://archive.coloradocycling.org']
 
 # Application definition
 
