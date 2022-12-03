e064c5214942bed545f284fa08d471c197514764
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-12-03 21:19:22 +0530

    html code push

--- wrh_brac/settings.py
+++ wrh_brac/settings.py
@@ -27,9 +27,9 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.232.188.93']
 
 # Application definition
 
