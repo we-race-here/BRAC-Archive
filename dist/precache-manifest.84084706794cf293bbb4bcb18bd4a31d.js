e72ed33b5ada5245d72fa50b7ad1f1df312dc89e
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-12-06 00:21:55 +0530

    #15

---
+++ dist/precache-manifest.84084706794cf293bbb4bcb18bd4a31d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c880acb4bf006cf70ba6fd883d61f7f9",
+    "url": "/cycling/static/CycleLogo.PNG"
+  },
+  {
+    "revision": "e63667b0e66162363bb3",
+    "url": "/cycling/static/css/chunk-vendors.df4cef94.css"
+  },
+  {
+    "revision": "989d8246e11a6caf6840563acfa4ffe8",
+    "url": "/cycling/static/img/brac_newlogo2.989d8246.png"
+  },
+  {
+    "revision": "b93957af6bd3d0ed82b3",
+    "url": "/cycling/static/js/app.028eb872.js"
+  },
+  {
+    "revision": "e63667b0e66162363bb3",
+    "url": "/cycling/static/js/chunk-vendors.1901b24c.js"
+  },
+  {
+    "revision": "a8aa6ceb03ceae5b18b344128c4947bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d986259c7629a9f625010fa1eab92df0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
