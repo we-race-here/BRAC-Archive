d503149ec1c074b125911c8acb8d8e5c44faf2b8
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2023-03-15 20:31:54 +0530

    added archive.bicyclecolorado.org in allowed host

--- wrh_brac/settings.py
+++ wrh_brac/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['165.232.188.93', 'http://archive.coloradocycling.org', '127.0.0.1', 'localhost', 'archive.coloradocycling.org']
+ALLOWED_HOSTS = ['165.232.188.93', 'http://archive.coloradocycling.org', '127.0.0.1', 'localhost', 'archive.coloradocycling.org','archive.bicyclecolorado.org']
 
 # Application definition
 
